00301f88de7b965c1e6aeeba655dd592730c6681
monissiddiqi <monissiddiqi123@gmail.com>
2025-05-15 16:43:03 +0530

    header fixed

--- frontend/app/(website)/_components/header.tsx
+++ frontend/app/(website)/_components/header.tsx
@@ -104,7 +104,7 @@ export const Header = () => {
         {/* <div className="hidden lg:flex">
           <UserProfile />
         </div> */}
-        <div className="w-44"></div>
+        <div className="lg:w-44"></div>
       </div>
     </header>
   );
